f36555ffccdb64f36a05c80913b6a1faf2a90ce9
Sergey Potapov <sergey.potapov@xbav.de>
2020-10-05 18:20:29 +0200

    Handle string message correctly

--- lib/logist/formatter/json.rb
+++ lib/logist/formatter/json.rb
@@ -9,29 +9,27 @@ module Logist
 
       def call(severity, timestamp, progname, msg)
         payload = { level: severity, timestamp: format_datetime(timestamp), environment: ::Rails.env }
+        normalized_msg = normalize_message(msg)
 
         if flat_json && msg.is_a?(Hash)
-          payload.merge!(msg)
+          payload.merge!(normalized_msg)
         else
-          payload.merge!(message: format_message(msg))
+          payload.merge!(message: normalized_msg)
         end
 
         payload.to_json << "\n"
       end
 
-      def format_message(msg)
-        case msg.class.name
-          when "Hash"
-            msg
-          when "Array"
-            msg
-          else
-            begin
-              JSON.parse(msg)
-            rescue JSON::ParserError
-              "#{msg}"
-            end
+      private
+
+      def normalize_message(msg)
+        if msg.is_a?(String)
+          JSON.parse(msg)
+        else
+          msg
         end
+      rescue JSON::ParserError
+        msg
       end
     end
   end
